f62ebe241274262cb0201322d3a69c1efd07a151
MercuryStudio812 <romanstyrev123457@gmail.com>
2026-07-07 21:35:32 +0500

    Fix manifest URL and enhance balance fetching logic
    Updated the manifest URL and improved the getBalance function to handle errors and display the balance correctly.

--- scripts/account.js
+++ scripts/account.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     const accountButton = document.querySelector(".account-button");
     const accountName = document.querySelector(".name-text");
+    const tonBalance = document.querySelector(".TON-quantity");
+    const simpleBalance = document.querySelector(".SIMPLE-quantity);
+                                
+    const connector = new TonConnectSDK.TonConnect({
+    manifestUrl: 'manifest.json'
+});
+    if (connector.connected) {
+        const wallet = connector.wallet;
+        getBalance(wallet.account.address);
+    }
 
     let userName = 'player';
 
@@ -11,12 +21,32 @@ document.addEventListener("DOMContentLoaded", () => {
             userName = user.username || user.first_name || 'player';
         }
     }
-
+    async function getBalance(address) {
+        try {
+            const response = await fetch(`https://tonapi.io/v2/accounts/${address}`);
+            const data = await response.json();
+            const balanceTON = (data.balance / 1_000_000_000).toFixed(2);
+            tonBalance.textContent = `${balanceTON} TON`;
+        } catch (error) {
+            tonBalance.textContent = 'Ошибка';
+        }
+    }
     function clickAccountButton() {
         document.getElementById("ld").style.display = "none";
         document.getElementById("main").style.display = "none";
         document.getElementById("acc").style.display = "block";
         accountName.textContent = userName;
+    }
+    async function TONWalletSign(){
+        try{
+            const wallets = await connector.getWallets();
+            await connector.connect(wallets[0]);
+            const wallet = connector.wallet;
+            const address = connector.address;
+            getBalance(wallet.account.address);
+        }catch(error){
+            console.log(error);
+        }
     }
 
     accountButton.addEventListener("click", clickAccountButton);
